7ada402453bd0fe20efb6dcb199ee18783aa1a5a
tneway <Tesfayedv@yahoo.com>
2024-06-19 15:35:10 -0500

    Update tbl.students.sql

--- prod/tbl.students.sql
+++ prod/tbl.students.sql
@@ -2,7 +2,7 @@
 Create table IF NOT EXISTS tasty_bytes_sample_data.raw_pos.tbl.STUDENTS(
 STUDENT_ID             INT,
 STUDENT_NAME           VARCHAR(50),
-AGE                    INT,
+AGE                    INT
 GRADE                  INT,
 GRADE_AVG              FLOAT
 )
