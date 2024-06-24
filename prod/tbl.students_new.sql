cbbb6500a8d346b17aa04b3f0d828b0ec62024ac
tesfaye neway <tesfayedv@gmail.com>
2024-06-24 09:22:51 -0500

    update sql LINTER

--- prod/tbl.students_new.sql
+++ prod/tbl.students_new.sql
@@ -1,9 +1,8 @@
-
 /* unique data will store in this 
    use password = 'kjlkjl7i8';
    use database = 'tasty_bytes_sample_data' */
-   
-CREATE OR REPLACE TABLE tasty_bytes_sample_data.raw_pos.tbl.STUDENTS_NEW(
+
+CREATE OR REPLACE TABLE TASTY_BYTES_SAMPLE_DATA.RAW_POS.TBL.STUDENTS_NEW(
 STUDENT_ID             INT,
 STUDENT_NAME           VARCHAR(50),
 AGE                    INT,
