49772cd3e07b20ed92d0dd424d41ac4f622c2e02
tesfaye neway <tesfayedv@gmail.com>
2024-06-27 08:23:06 -0500

    update a code

--- prod/tbl.students_new.sql
+++ prod/tbl.students_new.sql
@@ -3,8 +3,8 @@
    use database = 'tasty_bytes_sample_data' */
 CREATE OR REPLACE TABLE TASTY_BYTES_SAMPLE_DATA.RAW_POS.TBL.STUDENTS_NEW(
 STUDENT_ID             INT,
-STUDENT_NAME           VARCHAR(50),
-AGE                    INT,
+STUDENT_NAME           VARCHAR(50)
+age                    INT,
 GRADE                  INT,
 GRADE_AVG              FLOAT,
 DW_LOAD_DATE           DATETIME,
